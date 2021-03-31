dfaca98480e1cd7055a1d975aaa7af90d9eb7ffc
Dmitry Pushilin <dmitry.pushilin@gmail.com>
2021-03-31 17:51:51 +0300

    Add Flake8-WPS linter

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,8 +1,13 @@
+"""CLI module."""
 import prompt
 
+
 def greet():
+    """Greet function."""
     print('Welcome to the Brain Games!')
 
+
 def welcome_user():
+    """Welcome user function."""
     name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
+    print('Hello, {0}!'.format(name))
