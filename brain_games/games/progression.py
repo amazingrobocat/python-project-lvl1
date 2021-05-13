6a512011fc7d20b36599ae7f3833f4c8a8eccf06
Dmitry Pushilin <dmitry.pushilin@gmail.com>
2021-05-13 21:19:33 +0300

    Do step 8. Initial commit for brain-progression game

---
+++ brain_games/games/progression.py
@@ -0,0 +1,42 @@
+"""Brain-progression game module."""
+
+import random
+
+RULES = 'What number is missing in the progression?'
+
+NUMBER_MIN = 1
+NUMBER_MAX = 15
+STEP_MIN = 2
+STEP_MAX = 5
+LENGTH_MIN = 5
+LENGTH_MAX = 10
+
+
+def generate_progression_parts():
+    """Generate random arithmetic parameters of progression and progression itself.
+
+    Returns:
+        - arithmetic progression
+        - length parameter of progression
+    """
+    first_element = random.randint(NUMBER_MIN, NUMBER_MAX)
+    step = random.randint(STEP_MIN, STEP_MAX)
+    length = random.randint(LENGTH_MIN, LENGTH_MAX)
+    last_element = first_element + ((length - 1) * step)
+    progression = list(range(first_element, last_element + 1, step))
+    return progression, length
+
+
+def progression_game_module():
+    """Generate arithmetic progression with random hidden element.
+
+    Returns:
+        - question for the user(str)
+        - correct answer value(int)
+    """
+    progression, length = generate_progression_parts()
+    hidden_element = random.randint(0, (length - 1))
+    correct_answer = progression[hidden_element]
+    progression[hidden_element] = '..'
+    question = ' '.join([str(element) for element in progression])
+    return question, correct_answer
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""Brain-progression script."""
+from brain_games import progression_game_engine
+
+
+def main():
+    """Start brain-gcd game."""
+    progression_game_engine.game_progression()
+
+
+if __name__ == '__main__':
+    main()
