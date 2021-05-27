71b17ac94d7006566b91bc691effa627a7cc23ca
Dmitry Pushilin <dmitry.pushilin@gmail.com>
2021-05-28 00:23:51 +0300

    Refactor progression.py. Clean progression function

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -12,30 +12,40 @@ LENGTH_MIN = 5
 LENGTH_MAX = 10
 
 
-def generate_progression_parts():
-    """Generate random arithmetic parameters of progression and progression itself.
+def generate_progression(start, last, step):
+    """Generate arithmetic progression.
+
+    Args:
+        start(int): first number in arithmetic progression
+        last(int): last number in arithmetic progression
+        step(int): step between numbers in arithmetic progression
 
     Returns:
-        - arithmetic progression
-        - length parameter of progression
+        list of arithmetic progression
     """
-    first_element = random.randint(NUMBER_MIN, NUMBER_MAX)
-    step = random.randint(STEP_MIN, STEP_MAX)
-    length = random.randint(LENGTH_MIN, LENGTH_MAX)
-    last_element = first_element + ((length - 1) * step)
-    progression = list(range(first_element, last_element + 1, step))
-    return progression, length
+    return list(range(start, last, step))
 
 
 def launch_game_round():
-    """Generate arithmetic progression with random hidden element.
+    """Round of the Brain-progression game.
+
+    Generate random arithmetic parameters of progression.
+    Generate hidden parameter in arithmetic progression.
+    Generate arithmetic progression with random hidden element.
+    Calculate correct answer and question for a round of the game.
 
     Returns:
-        - question for the user(str)
-        - correct answer value(int)
+        question for the user(str)
+        correct answer value(int)
     """
-    progression, length = generate_progression_parts()
-    hidden_element = random.randint(0, (length - 1))
+    first_element = random.randint(NUMBER_MIN, NUMBER_MAX)
+    step_element = random.randint(STEP_MIN, STEP_MAX)
+    length_element = random.randint(LENGTH_MIN, LENGTH_MAX)
+    last_element = first_element + ((length_element - 1) * step_element)
+    progression = generate_progression(
+        first_element, last_element + 1, step_element,
+    )
+    hidden_element = random.randint(0, len(progression) - 1)
     correct_answer = progression[hidden_element]
     progression[hidden_element] = '..'
     progression_hidden = ' '.join([str(element) for element in progression])
