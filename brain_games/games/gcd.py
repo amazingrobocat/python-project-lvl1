47ac4ca72549c86327fdc5476d85e879767b779e
Dmitry Pushilin <dmitry.pushilin@gmail.com>
2021-05-27 19:45:44 +0300

    Refactor gcd.py. Remove excessive function

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -2,48 +2,43 @@
 import random
 
 INITIAL_FIRST_NUM = 1
-LAST_FIRST_NUM = 50
+LAST_FIRST_NUM = 45
 INITIAL_SECOND_NUM = 1
-LAST_SECOND_NUM = 20
+LAST_SECOND_NUM = 15
 RULES = 'Find the greatest common divisor of given numbers.'
 
 
-def generate_random_numbers():
-    """Generate two random numbers.
-
-    Returns:
-        two generated numbers.
-    """
-    first_number = random.randint(INITIAL_FIRST_NUM, LAST_FIRST_NUM)
-    second_number = random.randint(INITIAL_SECOND_NUM, LAST_SECOND_NUM)
-    return first_number, second_number
-
-
-def gcd(first_number, second_number):
+def gcd(number_one, number_two):
     """Find greatest common divisor of two numbers.
 
     Args:
-        first_number(int): first generated number.
-        second_number(int): second generated number.
+        number_one(int): first generated number
+        number_two(int): second generated number
 
     Returns:
-        GCD number.
+        GCD number
     """
-    while second_number != 0:
-        first_number, second_number = (
-            second_number, first_number % second_number,
+    while number_two != 0:
+        number_one, number_two = (
+            number_two, number_one % number_two,
         )
-    return first_number
+    return number_one
 
 
 def launch_game_round():
-    """Ask the user a question for a round of the game.
+    """Round of the Brain-GCD game.
+
+    Generate two random numbers.
+    Ask the user a question for a round of the game.
 
     Returns:
         - question for the user
         - correct answer value
     """
-    first_number, second_number = generate_random_numbers()
-    question = '{0} {1}'.format(first_number, second_number)
-    correct_answer = gcd(first_number, second_number)
+    first_generated_number = random.randint(INITIAL_FIRST_NUM, LAST_FIRST_NUM)
+    second_generated_number = random.randint(
+        INITIAL_SECOND_NUM, LAST_SECOND_NUM,
+    )
+    question = '{0} {1}'.format(first_generated_number, second_generated_number)
+    correct_answer = gcd(first_generated_number, second_generated_number)
     return question, correct_answer
